89164cb0a64701d2321c118a04e6001e937668f0
Martynas Deveikis <martynasdev@gmail.com>
2024-05-06 23:45:13 +0300

    [ADD] Project resource and data source testing, with some bugfixes

---
+++ internal/provider/project_resource_test.go
@@ -0,0 +1,115 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package provider
+
+import (
+	"fmt"
+	"github.com/hashicorp/terraform-plugin-testing/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-testing/terraform"
+	"os"
+	"regexp"
+	"strconv"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+)
+
+func TestAccProjectResource(t *testing.T) {
+	teamId := os.Getenv("CHERRY_TEST_TEAM_ID")
+	name := "terraform_test_project_" + acctest.RandString(5)
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheck(t) },
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		CheckDestroy:             testAccCheckCherryServersProjectDestroy,
+		Steps: []resource.TestStep{
+			// Create and Read testing
+			{
+				Config: testAccProjectResourceConfig(name, teamId),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					testAccCheckCherryServersProjectExists("cherryservers_project.test"),
+					resource.TestMatchResourceAttr("cherryservers_project.test", "href", regexp.MustCompile("/projects/[0-9]{6}")),
+					resource.TestCheckResourceAttr("cherryservers_project.test", "bgp.enabled", "false"),
+					resource.TestCheckResourceAttrSet("cherryservers_project.test", "bgp.local_asn"),
+					resource.TestMatchResourceAttr("cherryservers_project.test", "id", regexp.MustCompile("[0-9]{6}")),
+				),
+			},
+			// ImportState testing
+			{
+				ResourceName:            "cherryservers_project.test",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"team_id"},
+			},
+			// Update and Read testing
+			{
+				Config: testAccProjectResourceConfig(name+"_update", teamId),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("cherryservers_project.test", "name", name+"_update"),
+				),
+			},
+			// Delete testing automatically occurs in TestCase
+		},
+	})
+}
+
+func testAccProjectResourceConfig(name string, teamId string) string {
+	return fmt.Sprintf(`
+resource "cherryservers_project" "test" {
+  name = "%s"
+  team_id = "%s"
+}
+`, name, teamId)
+}
+
+func testAccCheckCherryServersProjectExists(resourceName string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[resourceName]
+
+		if !ok {
+			return fmt.Errorf("resource not found: %s", resourceName)
+		}
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("project ID is not set")
+		}
+		client := testCherryGoClient
+		projectID, err := strconv.Atoi(rs.Primary.ID)
+		if err != nil {
+			return fmt.Errorf("unable to convert Project ID")
+		}
+
+		// Try to get the project id
+		_, _, err = client.Projects.Get(projectID, nil)
+		if err != nil {
+			return err
+		}
+		return nil
+	}
+}
+
+func testAccCheckCherryServersProjectDestroy(s *terraform.State) error {
+	client := testCherryGoClient
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "cherryservers_project" {
+			continue
+		}
+
+		projectID, converr := strconv.Atoi(rs.Primary.ID)
+		if converr != nil {
+			return fmt.Errorf("unable to convert Project ID")
+		}
+		// Try to get the project
+		_, resp, err := client.Projects.Get(projectID, nil)
+
+		if err != nil {
+			if is404Error(resp) {
+				return nil
+			}
+
+			return fmt.Errorf("project listing error: %#v", err)
+		}
+	}
+
+	return nil
+}
--- internal/provider/provider_test.go
+++ internal/provider/provider_test.go
@@ -4,22 +4,49 @@
 package provider
 
 import (
+	"fmt"
+	"github.com/cherryservers/cherrygo/v3"
+	"os"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
 	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
 )
 
+// TODO
+// Use a provider instead of a client, when that functionality is available
+var testCherryGoClient *cherrygo.Client
+
 // testAccProtoV6ProviderFactories are used to instantiate a provider during
 // acceptance testing. The factory function will be invoked for every Terraform
 // CLI command executed to create a provider server to which the CLI can
 // reattach.
 var testAccProtoV6ProviderFactories = map[string]func() (tfprotov6.ProviderServer, error){
-	"scaffolding": providerserver.NewProtocol6WithError(New("test")()),
+	"cherryservers": providerserver.NewProtocol6WithError(New("test")()),
 }
 
 func testAccPreCheck(t *testing.T) {
 	// You can add code here to run prior to any test case execution, for example assertions
 	// about the appropriate environment variables being set are common to see in a pre-check
 	// function.
+	if v := os.Getenv("CHERRY_AUTH_KEY"); v == "" {
+		t.Fatal("CHERRY_AUTH_KEY must be set for acceptance tests")
+	}
+	if v := os.Getenv("CHERRY_TEST_TEAM_ID"); v == "" {
+		t.Fatal("CHERRY_TEST_TEAM_ID must be set for acceptance tests")
+	}
+	if v := os.Getenv("CHERRY_TEST_PROJECT_ID"); v == "" {
+		t.Fatal("CHERRY_TEST_PROJECT_ID must be set for acceptance tests")
+	}
+
+	//TODO
+	//Make version responsive (or better yet, use a provider instead of a client)
+	userAgent := fmt.Sprintf("terraform-provider/cherryservers/%s terraform/%s", "test", "1.0.0")
+	args := []cherrygo.ClientOpt{cherrygo.WithAuthToken(os.Getenv("CHERRY_AUTH_KEY")), cherrygo.WithUserAgent(userAgent)}
+	client, err := cherrygo.NewClient(args...)
+	if err != nil {
+		t.Fatal("error: couldn't create client a cherry servers client for testing:", err)
+	}
+	testCherryGoClient = client
+
 }
