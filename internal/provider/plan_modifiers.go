dec0a30376f5f88a6391de175455f4f0516e1704
Martynas Deveikis <martynasdev@gmail.com>
2024-05-22 05:21:30 +0300

    [CHANGE] IP resource attributes unknown state setting

--- internal/provider/plan_modifiers.go
+++ internal/provider/plan_modifiers.go
@@ -7,40 +7,42 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/types"
 )
 
-var _ planmodifier.String = unknownDependingOnUpdateAttributePlanModifier{}
+var _ planmodifier.String = useStateIfNoConfigurationChangesAttributePlanModifier{}
 
-// UnknownDependingOnUpdateAttributePlanModifier marks attribute as unknown if any of the `attributePaths` are being updated.
-func UnknownDependingOnUpdateAttributePlanModifier(attributePaths []string) planmodifier.String {
-	return &unknownDependingOnUpdateAttributePlanModifier{
+// UseStateIfNoConfigurationChangesAttributePlanModifier matches a computed attribute to its state if there are no
+// configuration changes in `attributePaths`. `attributePaths` should consist of required, optional or optional&computed attributes.
+func UseStateIfNoConfigurationChangesAttributePlanModifier(attributePaths []string) planmodifier.String {
+	return &useStateIfNoConfigurationChangesAttributePlanModifier{
 		attributePaths: attributePaths,
 	}
 }
 
-type unknownDependingOnUpdateAttributePlanModifier struct {
+type useStateIfNoConfigurationChangesAttributePlanModifier struct {
 	attributePaths []string
 }
 
-func (d unknownDependingOnUpdateAttributePlanModifier) Description(ctx context.Context) string {
-	return "Marks attribute as unknown if any of the attributePaths are being updated"
+func (d useStateIfNoConfigurationChangesAttributePlanModifier) Description(ctx context.Context) string {
+	return "Matches attribute plan to state if the practitioner has not updated the configuration."
 }
 
-func (d unknownDependingOnUpdateAttributePlanModifier) MarkdownDescription(ctx context.Context) string {
+func (d useStateIfNoConfigurationChangesAttributePlanModifier) MarkdownDescription(ctx context.Context) string {
 	return d.Description(ctx)
 }
 
-func (d unknownDependingOnUpdateAttributePlanModifier) PlanModifyString(ctx context.Context, req planmodifier.StringRequest, resp *planmodifier.StringResponse) {
+func (d useStateIfNoConfigurationChangesAttributePlanModifier) PlanModifyString(ctx context.Context, req planmodifier.StringRequest, resp *planmodifier.StringResponse) {
 	//Ignore create or destroy cases.
 	if req.State.Raw.IsNull() || req.Plan.Raw.IsNull() {
 		return
 	}
 
-	if !req.PlanValue.IsUnknown() {
+	//Ignore cases where the attribute has been configured.
+	if !req.ConfigValue.IsNull() {
 		return
 	}
 
 	for _, attributePath := range d.attributePaths {
-		var attributePlan types.String
-		diags := req.Plan.GetAttribute(ctx, path.Root(attributePath), &attributePlan)
+		var attributeConfig types.String
+		diags := req.Config.GetAttribute(ctx, path.Root(attributePath), &attributeConfig)
 		resp.Diagnostics.Append(diags...)
 		if resp.Diagnostics.HasError() {
 			return
@@ -53,11 +55,11 @@ func (d unknownDependingOnUpdateAttributePlanModifier) PlanModifyString(ctx cont
 			return
 		}
 
-		if (attributePlan.IsNull() && !attributeState.IsNull()) || (!attributePlan.IsNull() && attributeState.IsNull()) ||
-			attributePlan.IsUnknown() || attributePlan.ValueString() != attributeState.ValueString() {
+		if !attributeConfig.IsNull() && !attributeState.Equal(attributeConfig) {
 			resp.PlanValue = types.StringUnknown()
 			return
 		}
+
 	}
 
 	resp.PlanValue = req.StateValue
