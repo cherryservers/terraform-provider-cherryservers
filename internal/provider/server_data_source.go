a82923e0a50c2dc080bf0e1fb88a167392cf25f0
Martynas Deveikis <martynasdev@gmail.com>
2024-05-23 08:17:10 +0300

    [CHANGE] restructure server files

--- internal/provider/server_data_source.go
+++ internal/provider/server_data_source.go
@@ -4,9 +4,13 @@ import (
 	"context"
 	"fmt"
 	"github.com/cherryservers/cherrygo/v3"
+	"github.com/hashicorp/terraform-plugin-framework-timeouts/datasource/timeouts"
 	"github.com/hashicorp/terraform-plugin-framework-validators/datasourcevalidator"
 	"github.com/hashicorp/terraform-plugin-framework/datasource"
+	"github.com/hashicorp/terraform-plugin-framework/datasource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/diag"
 	"github.com/hashicorp/terraform-plugin-framework/path"
+	"github.com/hashicorp/terraform-plugin-framework/types"
 	"github.com/hashicorp/terraform-plugin-log/tflog"
 	"strconv"
 )
@@ -27,6 +31,55 @@ type serverDataSource struct {
 	client *cherrygo.Client
 }
 
+type serverDataSourceModel struct {
+	Plan                types.String   `tfsdk:"plan"`
+	ProjectId           types.Int64    `tfsdk:"project_id"`
+	Region              types.String   `tfsdk:"region"`
+	Hostname            types.String   `tfsdk:"hostname"`
+	Name                types.String   `tfsdk:"name"`
+	Username            types.String   `tfsdk:"username"`
+	Password            types.String   `tfsdk:"password"`
+	BMC                 types.Object   `tfsdk:"bmc"`
+	Image               types.String   `tfsdk:"image"`
+	SSHKeyIds           types.Set      `tfsdk:"ssh_key_ids"`
+	ExtraIPAddressesIds types.Set      `tfsdk:"extra_ip_addresses_ids"`
+	UserDataFile        types.String   `tfsdk:"user_data_file"`
+	Tags                types.Map      `tfsdk:"tags"`
+	SpotInstance        types.Bool     `tfsdk:"spot_instance"`
+	OSPartitionSize     types.Int64    `tfsdk:"os_partition_size"`
+	PowerState          types.String   `tfsdk:"power_state"`
+	State               types.String   `tfsdk:"state"`
+	IpAddresses         types.Set      `tfsdk:"ip_addresses"`
+	Id                  types.String   `tfsdk:"id"`
+	Timeouts            timeouts.Value `tfsdk:"timeouts"`
+}
+
+func (d *serverDataSourceModel) populateModel(server cherrygo.Server, ctx context.Context, diags diag.Diagnostics, powerState string) {
+	var resourceModel serverResourceModel
+	resourceModel.populateModel(server, ctx, diags, powerState)
+
+	d.Plan = resourceModel.Plan
+	d.ProjectId = resourceModel.ProjectId
+	d.Region = resourceModel.Region
+	d.Hostname = resourceModel.Hostname
+	d.Name = resourceModel.Name
+	d.Username = resourceModel.Username
+	d.Password = resourceModel.Password
+	d.BMC = resourceModel.BMC
+	d.Image = resourceModel.Image
+	d.SSHKeyIds = resourceModel.SSHKeyIds
+	//d.ExtraIPAddressesIds = resourceModel.ExtraIPAddressesIds
+	d.UserDataFile = resourceModel.UserDataFile
+	d.Tags = resourceModel.Tags
+	d.SpotInstance = resourceModel.SpotInstance
+	d.OSPartitionSize = resourceModel.OSPartitionSize
+	d.PowerState = resourceModel.PowerState
+	d.State = resourceModel.State
+	d.IpAddresses = resourceModel.IpAddresses
+	d.Id = resourceModel.Id
+
+}
+
 func (d *serverDataSource) Metadata(ctx context.Context, req datasource.MetadataRequest, resp *datasource.MetadataResponse) {
 	resp.TypeName = req.ProviderTypeName + "_server"
 }
@@ -40,7 +93,129 @@ func (d *serverDataSource) ConfigValidators(ctx context.Context) []datasource.Co
 }
 
 func (d *serverDataSource) Schema(ctx context.Context, req datasource.SchemaRequest, resp *datasource.SchemaResponse) {
-	resp.Schema = serverDataSourceSchema(ctx)
+	resp.Schema = schema.Schema{
+		Description: "Provides a Cherry Servers server resource. This can be used to create, read, modify, and delete servers on your Cherry Servers account.",
+
+		Attributes: map[string]schema.Attribute{
+			"plan": schema.StringAttribute{
+				Computed:    true,
+				Description: "Slug of the plan. Example: e5_1620v4. [See List Plans](https://api.cherryservers.com/doc/#tag/Plans/operation/get-plans).",
+			},
+			"project_id": schema.Int64Attribute{
+				Description: "CherryServers project id, associated with the server.",
+				Computed:    true,
+				Optional:    true,
+			},
+			"region": schema.StringAttribute{
+				Description: "Slug of the region. Example: eu_nord_1 [See List Regions](https://api.cherryservers.com/doc/#tag/Regions/operation/get-regions).",
+				Computed:    true,
+			},
+			"name": schema.StringAttribute{
+				Description: "Name of the server.",
+				Computed:    true,
+			},
+			"hostname": schema.StringAttribute{
+				Description: "Hostname of the server.",
+				Computed:    true,
+				Optional:    true,
+			},
+			"username": schema.StringAttribute{
+				Description: "Server username credential.",
+				Computed:    true,
+			},
+			"password": schema.StringAttribute{
+				Description: "Server password credential.",
+				Computed:    true,
+				Sensitive:   true,
+			},
+			"bmc": schema.SingleNestedAttribute{
+				Description: "Server BMC credentials.",
+				Computed:    true,
+				Attributes: map[string]schema.Attribute{
+					"user": schema.StringAttribute{
+						Computed: true,
+					},
+					"password": schema.StringAttribute{
+						Computed:  true,
+						Sensitive: true,
+					},
+				},
+			},
+			"image": schema.StringAttribute{
+				Description: "Slug of the operating system. Example: ubuntu_22_04. [See List Images](https://api.cherryservers.com/doc/#tag/Images/operation/get-plan-images).",
+				Computed:    true,
+			},
+			"ssh_key_ids": schema.SetAttribute{
+				Description: "Set of the SSH key IDs allowed to SSH to the server.",
+				Computed:    true,
+				ElementType: types.StringType,
+			},
+			"extra_ip_addresses_ids": schema.SetAttribute{
+				Description: "Set of the IP address IDs to be embedded into the Server.",
+				Computed:    true,
+				ElementType: types.StringType,
+			},
+			"user_data_file": schema.StringAttribute{
+				Description: "Base64 encoded User-Data blob. It should be either a bash or cloud-config script.",
+				Computed:    true,
+			},
+			"tags": schema.MapAttribute{
+				Description: "Key/value metadata for server tagging.",
+				ElementType: types.StringType,
+				Computed:    true,
+			},
+			"spot_instance": schema.BoolAttribute{
+				Description: "If True, provisions the server as a spot instance.",
+				Computed:    true,
+			},
+			"os_partition_size": schema.Int64Attribute{
+				Description: "OS partition size in GB.",
+				Computed:    true,
+			},
+			"power_state": schema.StringAttribute{
+				Description: "The power state of the server, such as 'Powered off' or 'Powered on'.",
+				Computed:    true,
+			},
+			"state": schema.StringAttribute{
+				Description: "The state of the server, such as 'pending' or 'active'.",
+				Computed:    true,
+			},
+			"ip_addresses": schema.SetNestedAttribute{
+				Description: "IP addresses attached to the server.",
+				Computed:    true,
+				NestedObject: schema.NestedAttributeObject{
+					Attributes: map[string]schema.Attribute{
+						"id": schema.StringAttribute{
+							Description: "ID of the IP address.",
+							Computed:    true,
+						},
+						"type": schema.StringAttribute{
+							Description: "Type of the IP address.",
+							Computed:    true,
+						},
+						"address": schema.StringAttribute{
+							Description: "Address of the IP address.",
+							Computed:    true,
+						},
+						"address_family": schema.Int64Attribute{
+							Description: "Address family of the IP address.",
+							Computed:    true,
+						},
+						"cidr": schema.StringAttribute{
+							Description: "CIDR of the IP address.",
+							Computed:    true,
+						},
+					},
+				},
+			},
+			"id": schema.StringAttribute{
+				Optional:    true,
+				Computed:    true,
+				Description: "Server identifier.",
+			},
+			"timeouts": timeouts.Attributes(ctx),
+		},
+	}
 }
 
 func (d *serverDataSource) Configure(ctx context.Context, req datasource.ConfigureRequest, resp *datasource.ConfigureResponse) {
--- internal/provider/server_resource.go
+++ internal/provider/server_resource.go
@@ -6,8 +6,22 @@ import (
 	"errors"
 	"github.com/cenkalti/backoff/v4"
 	"github.com/cherryservers/cherrygo/v3"
+	"github.com/hashicorp/terraform-plugin-framework-timeouts/resource/timeouts"
+	"github.com/hashicorp/terraform-plugin-framework/attr"
+	"github.com/hashicorp/terraform-plugin-framework/diag"
 	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/booldefault"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/boolplanmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/int64planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/mapdefault"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/mapplanmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/objectplanmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/setplanmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/types"
 	"github.com/hashicorp/terraform-plugin-log/tflog"
 	"os"
 	"strconv"
@@ -30,12 +44,347 @@ type serverResource struct {
 	client *cherrygo.Client
 }
 
+// serverResourceModel describes the resource data model.
+type serverResourceModel struct {
+	Plan                types.String   `tfsdk:"plan"`
+	ProjectId           types.Int64    `tfsdk:"project_id"`
+	Region              types.String   `tfsdk:"region"`
+	Hostname            types.String   `tfsdk:"hostname"`
+	Name                types.String   `tfsdk:"name"`
+	Username            types.String   `tfsdk:"username"`
+	Password            types.String   `tfsdk:"password"`
+	BMC                 types.Object   `tfsdk:"bmc"`
+	Image               types.String   `tfsdk:"image"`
+	SSHKeyIds           types.Set      `tfsdk:"ssh_key_ids"`
+	ExtraIPAddressesIds types.Set      `tfsdk:"extra_ip_addresses_ids"`
+	UserDataFile        types.String   `tfsdk:"user_data_file"`
+	Tags                types.Map      `tfsdk:"tags"`
+	SpotInstance        types.Bool     `tfsdk:"spot_instance"`
+	OSPartitionSize     types.Int64    `tfsdk:"os_partition_size"`
+	PowerState          types.String   `tfsdk:"power_state"`
+	State               types.String   `tfsdk:"state"`
+	IpAddresses         types.Set      `tfsdk:"ip_addresses"`
+	Id                  types.String   `tfsdk:"id"`
+	Timeouts            timeouts.Value `tfsdk:"timeouts"`
+}
+
+func (d *serverResourceModel) populateModel(server cherrygo.Server, ctx context.Context, diags diag.Diagnostics, powerState string) {
+	d.Plan = types.StringValue(server.Plan.Slug)
+	d.ProjectId = types.Int64Value(int64(server.Project.ID))
+	d.Region = types.StringValue(server.Region.Slug)
+	d.Hostname = types.StringValue(server.Hostname)
+	d.Name = types.StringValue(server.Name)
+	d.Username = types.StringValue(server.Username)
+	d.Password = types.StringValue(server.Password)
+
+	bmcModel := bmcResourceModel{
+		User:     types.StringValue(server.BMC.User),
+		Password: types.StringValue(server.BMC.Password),
+	}
+	bmcTf, bmcDiags := types.ObjectValueFrom(ctx, bmcModel.AttributeTypes(), bmcModel)
+	diags.Append(bmcDiags...)
+
+	d.BMC = bmcTf
+
+	//d.Image = types.StringValue(server.Image)
+
+	//var sshKeyIds, ipIds []string
+	var sshKeyIds []string
+	for _, sshKey := range server.SSHKeys {
+		sshKeyID := strconv.Itoa(sshKey.ID)
+		sshKeyIds = append(sshKeyIds, sshKeyID)
+	}
+	sshKeyIdsTf, sshDiags := types.SetValueFrom(ctx, types.StringType, sshKeyIds)
+	d.SSHKeyIds = sshKeyIdsTf
+	diags.Append(sshDiags...)
+
+	var ips []attr.Value
+	for _, ip := range server.IPAddresses {
+
+		// ExtraIPAddresses shouldn't have unmodifiable (primary and private type) IPs
+		/*if ip.Type == "subnet" || ip.Type == "floating-ip" {
+			ipIds = append(ipIds, ip.ID)
+		}*/
+
+		ipModel := ipAddressFlatResourceModel{
+			Id:            types.StringValue(ip.ID),
+			Type:          types.StringValue(ip.Type),
+			Address:       types.StringValue(ip.Address),
+			AddressFamily: types.Int64Value(int64(ip.AddressFamily)),
+			CIDR:          types.StringValue(ip.Cidr),
+		}
+
+		ipTf, ipDiags := types.ObjectValueFrom(ctx, ipModel.AttributeTypes(), ipModel)
+		diags.Append(ipDiags...)
+
+		ips = append(ips, ipTf)
+	}
+
+	ipsTf, ipsDiags := types.SetValue(types.ObjectType{AttrTypes: ipAddressFlatResourceModel{}.AttributeTypes()}, ips)
+	diags.Append(ipsDiags...)
+	d.IpAddresses = ipsTf
+
+	/*ipIdsTf, ipIdDiags := types.SetValueFrom(ctx, types.StringType, ipIds)
+	d.ExtraIPAddressesIds = ipIdsTf
+	diags.Append(ipIdDiags...)*/
+
+	tags, tagsDiags := types.MapValueFrom(ctx, types.StringType, server.Tags)
+	d.Tags = tags
+	diags.Append(tagsDiags...)
+
+	d.SpotInstance = types.BoolValue(server.SpotInstance)
+	d.PowerState = types.StringValue(powerState)
+	d.State = types.StringValue(server.State)
+	d.Id = types.StringValue(strconv.Itoa(server.ID))
+}
+
+type ipAddressFlatResourceModel struct {
+	Id            types.String `tfsdk:"id"`
+	Type          types.String `tfsdk:"type"`
+	Address       types.String `tfsdk:"address"`
+	AddressFamily types.Int64  `tfsdk:"address_family"`
+	CIDR          types.String `tfsdk:"cidr"`
+}
+
+func (m ipAddressFlatResourceModel) AttributeTypes() map[string]attr.Type {
+	return map[string]attr.Type{
+		"id":             types.StringType,
+		"type":           types.StringType,
+		"address":        types.StringType,
+		"address_family": types.Int64Type,
+		"cidr":           types.StringType,
+	}
+}
+
+type bmcResourceModel struct {
+	User     types.String `tfsdk:"user"`
+	Password types.String `tfsdk:"password"`
+}
+
+func (m bmcResourceModel) AttributeTypes() map[string]attr.Type {
+	return map[string]attr.Type{
+		"user":     types.StringType,
+		"password": types.StringType,
+	}
+}
+
 func (r *serverResource) Metadata(ctx context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
 	resp.TypeName = req.ProviderTypeName + "_server"
 }
 
 func (r *serverResource) Schema(ctx context.Context, req resource.SchemaRequest, resp *resource.SchemaResponse) {
-	resp.Schema = serverResourceSchema(ctx)
+	resp.Schema = schema.Schema{
+		// This description is used by the documentation generator and the language server.
+		Description: "Provides a Cherry Servers server resource. This can be used to create, read, modify, and delete servers on your Cherry Servers account.",
+
+		Attributes: map[string]schema.Attribute{
+			"plan": schema.StringAttribute{
+				Required: true,
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.RequiresReplace(),
+				},
+				Description: "Slug of the plan. Example: e5_1620v4. [See List Plans](https://api.cherryservers.com/doc/#tag/Plans/operation/get-plans).",
+			},
+			"project_id": schema.Int64Attribute{
+				Description: "CherryServers project id, associated with the server.",
+				Required:    true,
+				PlanModifiers: []planmodifier.Int64{
+					int64planmodifier.RequiresReplace(),
+				},
+			},
+			"region": schema.StringAttribute{
+				Description: "Slug of the region. Example: eu_nord_1 [See List Regions](https://api.cherryservers.com/doc/#tag/Regions/operation/get-regions).",
+				Required:    true,
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.RequiresReplace(),
+				},
+			},
+			"name": schema.StringAttribute{
+				Description: "Name of the server.",
+				Optional:    true,
+				Computed:    true,
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.UseStateForUnknown(),
+				},
+			},
+			"hostname": schema.StringAttribute{
+				Description: "Hostname of the server.",
+				Optional:    true,
+				Computed:    true,
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.UseStateForUnknown(),
+				},
+			},
+			"username": schema.StringAttribute{
+				Description: "Server username credential.",
+				Computed:    true,
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.UseStateForUnknown(),
+				},
+			},
+			"password": schema.StringAttribute{
+				Description: "Server password credential.",
+				Computed:    true,
+				Sensitive:   true,
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.UseStateForUnknown(),
+				},
+			},
+			"bmc": schema.SingleNestedAttribute{
+				Description: "Server BMC credentials.",
+				Computed:    true,
+				PlanModifiers: []planmodifier.Object{
+					objectplanmodifier.UseStateForUnknown(),
+				},
+				Attributes: map[string]schema.Attribute{
+					"user": schema.StringAttribute{
+						Computed: true,
+						PlanModifiers: []planmodifier.String{
+							stringplanmodifier.UseStateForUnknown(),
+						},
+					},
+					"password": schema.StringAttribute{
+						Computed:  true,
+						Sensitive: true,
+						PlanModifiers: []planmodifier.String{
+							stringplanmodifier.UseStateForUnknown(),
+						},
+					},
+				},
+			},
+			"image": schema.StringAttribute{
+				Description: "Slug of the operating system. Example: ubuntu_22_04. [See List Images](https://api.cherryservers.com/doc/#tag/Images/operation/get-plan-images).",
+				Optional:    true,
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.RequiresReplace(),
+				},
+			},
+			"ssh_key_ids": schema.SetAttribute{
+				Description: "Set of the SSH key IDs allowed to SSH to the server.",
+				Optional:    true,
+				ElementType: types.StringType,
+				PlanModifiers: []planmodifier.Set{
+					setplanmodifier.RequiresReplace(),
+				},
+			},
+			"extra_ip_addresses_ids": schema.SetAttribute{
+				Description: "Set of the IP address IDs to be embedded into the server.",
+				Optional:    true,
+				ElementType: types.StringType,
+				PlanModifiers: []planmodifier.Set{
+					setplanmodifier.RequiresReplace(),
+				},
+			},
+			"user_data_file": schema.StringAttribute{
+				Description: "Base64 encoded User-Data blob. It should be either a bash or cloud-config script.",
+				Optional:    true,
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.RequiresReplace(),
+				},
+			},
+			"tags": schema.MapAttribute{
+				Description: "Key/value metadata for server tagging.",
+				Optional:    true,
+				ElementType: types.StringType,
+				Default:     mapdefault.StaticValue(types.MapValueMust(types.StringType, map[string]attr.Value{})),
+				Computed:    true,
+				PlanModifiers: []planmodifier.Map{
+					mapplanmodifier.UseStateForUnknown(),
+				},
+			},
+			"spot_instance": schema.BoolAttribute{
+				Description: "If True, provisions the server as a spot instance.",
+				Optional:    true,
+				Computed:    true,
+				Default:     booldefault.StaticBool(false),
+				PlanModifiers: []planmodifier.Bool{
+					boolplanmodifier.RequiresReplace(),
+					boolplanmodifier.UseStateForUnknown(),
+				},
+			},
+			"os_partition_size": schema.Int64Attribute{
+				Description: "OS partition size in GB.",
+				Optional:    true,
+				PlanModifiers: []planmodifier.Int64{
+					int64planmodifier.RequiresReplace(),
+				},
+			},
+			"power_state": schema.StringAttribute{
+				Description: "The power state of the server, such as 'Powered off' or 'Powered on'.",
+				Computed:    true,
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.UseStateForUnknown(),
+					stringplanmodifier.RequiresReplace(),
+				},
+			},
+			"state": schema.StringAttribute{
+				Description: "The state of the server, such as 'pending' or 'active'.",
+				Computed:    true,
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.UseStateForUnknown(),
+				},
+			},
+			"ip_addresses": schema.SetNestedAttribute{
+				Description: "IP addresses attached to the server.",
+				PlanModifiers: []planmodifier.Set{
+					setplanmodifier.UseStateForUnknown(),
+					//IpAddressDependantOnExtraModifier(),
+					//TODO Use state for unknown if no extra addresses
+				},
+				Computed: true,
+				NestedObject: schema.NestedAttributeObject{
+					Attributes: map[string]schema.Attribute{
+						"id": schema.StringAttribute{
+							Description: "ID of the IP address.",
+							Computed:    true,
+							PlanModifiers: []planmodifier.String{
+								stringplanmodifier.UseStateForUnknown(),
+							},
+						},
+						"type": schema.StringAttribute{
+							Description: "Type of the IP address.",
+							Computed:    true,
+							PlanModifiers: []planmodifier.String{
+								stringplanmodifier.UseStateForUnknown(),
+							},
+						},
+						"address": schema.StringAttribute{
+							Description: "Address of the IP address.",
+							Computed:    true,
+							PlanModifiers: []planmodifier.String{
+								stringplanmodifier.UseStateForUnknown(),
+							},
+						},
+						"address_family": schema.Int64Attribute{
+							Description: "Address family of the IP address.",
+							Computed:    true,
+							PlanModifiers: []planmodifier.Int64{
+								int64planmodifier.UseStateForUnknown(),
+							},
+						},
+						"cidr": schema.StringAttribute{
+							Description: "CIDR of the IP address.",
+							Computed:    true,
+							PlanModifiers: []planmodifier.String{
+								stringplanmodifier.UseStateForUnknown(),
+							},
+						},
+					},
+				},
+			},
+			"id": schema.StringAttribute{
+				Computed:    true,
+				Description: "Server identifier.",
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.UseStateForUnknown(),
+				},
+			},
+			"timeouts": timeouts.Attributes(ctx, timeouts.Opts{
+				Create: true,
+				Update: true,
+			}),
+		},
+	}
 }
 
 func (r *serverResource) Configure(ctx context.Context, req resource.ConfigureRequest, resp *resource.ConfigureResponse) {
