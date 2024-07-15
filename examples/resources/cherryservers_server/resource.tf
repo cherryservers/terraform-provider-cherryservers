320c380018b159fed9957206f18a3aa6556007ee
Martynas Deveikis <martynasdev@gmail.com>
2024-07-15 16:38:36 +0300

    fix-server-docs

--- examples/resources/cherryservers_server/resource.tf
+++ examples/resources/cherryservers_server/resource.tf
@@ -11,9 +11,9 @@ resource "cherryservers_server" "server" {
   hostname         = "sharing-wallaby"
   project_id       = 123456
   region           = "eu_nord_1"
-  image            = "ubuntu_22_04"
+  image_slug            = "ubuntu_22_04"
   ssh_key_ids      = ["1", "2"]
-  ip_addresses_ids = ["8269de5d-9b89-af9a-8bcc-8efb4d9fa282"]
+  extra_ip_addresses_ids = ["8269de5d-9b89-af9a-8bcc-8efb4d9fa282"]
   spot_instance    = true
   tags = {
     Name        = "Example Instance"
